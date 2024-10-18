19b61af97fffe3b2fefc7c0838fc83342add89d0
Mezzky <ryansahadha@gmail.com>
2024-10-19 01:08:05 +0800

    Add: New Section and Video Feature

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,53 +1,56 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-    screens: {
-      'sm': '320px',
-      'md': '375px',
-      'lg': '425px',
-      'tablet': '768px',
-      'laptop': '1024px',
-      'desktop': '1280px',
+    darkMode: ['class'],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+		theme: {
+    	screens: {
+    		sm: '320px',
+    		md: '375px',
+    		lg: '425px',
+    		tablet: '768px',
+    		laptop: '1024px',
+    		desktop: '1280px'
+    	},
+    	extend: {
+    		fontFamily: {
+    			esamanru: 'Esamanru',
+    			gmarket: 'Gmarket Sans'
+    		},
+    		colors: {
+    			main: '#F8D55B',
+    			secondary: '#75B9C9',
+    			third: '#EF3340',
+    			dark: '#5C3217',
+    			black: '#2D2926'
+    		},
+    		backgroundImage: {
+    			'home-bg': "url('/image/section1-bg.png')",
+    		},
+    		gridTemplateColumns: {
+    			'custom-70-30': '70% 30%'
+    		},
+    		textShadow: {
+    			sm: '1px 1px 2px rgba(0, 0, 0, 0.5)',
+    			md: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+    			lg: '3px 3px 6px rgba(0, 0, 0, 0.5)',
+    			xl: '4px 4px 8px rgba(0, 0, 0, 0.5)'
+    		},
+    		textStrokeWidth: {
+    			'1': '1px',
+    			'2': '2px',
+    			'3': '3px'
+    		},
+    		textStrokeColor: {
+    			dark: '#5C3217',
+    			white: '#fff'
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
     },
-    extend: {
-      fontFamily: {
-        esamanru: 'Esamanru',
-        gmarket: 'Gmarket Sans',
-      },
-      colors: {
-        main: '#F8D55B',
-        secondary: '#75B9C9',
-        third: '#EF3340',
-        dark: '#5C3217',
-        black: '#2D2926',
-      },
-      backgroundImage: {
-        'home-bg': "url('/image/section1-bg.png')",
-      },
-      gridTemplateColumns: {
-        // Add your custom grid here
-        'custom-70-30': '70% 30%', // Custom grid with 70% and 30% width
-      },
-      // Add text shadow utility
-      textShadow: {
-        'sm': '1px 1px 2px rgba(0, 0, 0, 0.5)',
-        'md': '2px 2px 4px rgba(0, 0, 0, 0.5)',
-        'lg': '3px 3px 6px rgba(0, 0, 0, 0.5)',
-        'xl': '4px 4px 8px rgba(0, 0, 0, 0.5)',
-      },
-      // Add text stroke utility
-      textStrokeWidth: {
-        '1': '1px',
-        '2': '2px',
-        '3': '3px',
-      },
-      textStrokeColor: {
-        'dark': '#5C3217',
-        'white': '#fff',
-      },
-    },
-	},
 	plugins: [
     // Plugin for text shadow and text stroke
     function ({ addUtilities }) {
@@ -65,6 +68,7 @@ export default {
         '.text-stroke-white': { '-webkit-text-stroke-color': '#fff' },
       };
       addUtilities(newUtilities);
-    }
-  ],
+    },
+        require("tailwindcss-animate")
+    ],
 };
